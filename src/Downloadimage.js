6af56b203fd226e5e6872bccfbfd6b6634ee34ac
Glitch (reactpoc1) <none>
2019-10-22 11:59:22 +0000

    🏈🎎 Updated with Glitch on 10/22/2019

--- src/Downloadimage.js
+++ src/Downloadimage.js
@@ -3,8 +3,15 @@ import './App.css';
 import axios from 'axios';
 
 var idname ='';
+var persons='' ;
 
-export default class PopupWOLib extends Component {
+export default class Downloadimage extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={ filenames : null, render: false
+    };
+    this.getfilenames=this.getfilenames.bind(this);
+}
 
     download(filename){
 
@@ -20,21 +27,51 @@ export default class PopupWOLib extends Component {
        idname = document.getElementById(filename),
        idname.href="http://localhost:8000/download/",
       )}
+
+      componentDidMount(){
+        console.log(1);
+        this.getfilenames();
+      }
+      getfilenames(){
+        axios.get("http://localhost:8000/getfiles")
+        .then(res=>{    persons = res.data;
+                       
+        })
+        .then(()=>{ 
+            this.setState({filenames: persons,render:true})
+            // console.log(this.state.filenames)
+            // console.log(this.state.filenames[0]) 
+            // console.log(this.state.filenames.length) 
+            
+        })      
+      }
     
     render() {
         console.log(window.location.href)
-        var filenames =this.props.fnames
+       // var filenames =this.props.fnames
+       var boolfromupload =this.props.frompopup 
+       var filenames =this.state.filenames
+       console.log(filenames)
         // eslint-disable-next-line
-        var listItems = filenames.map((filename) => <a href= "#" id={filename} key={filename.toString()} onClick={()=>this.download(filename)} > <li > {filename} </li></a>  )
+        // var listItems = filenames.map((filename) => <a href= "#" id={filename} key={filename.toString()} onClick={()=>this.download(filename)} > <li > {filename} </li></a>  )
         
       return (
-        <div className='popup'>
-          <div className='popup_inner'>
+        <div className='download-main'>
+
+        { this.state.render && !boolfromupload &&   
+          <div className='download_inner'>
             <h1 style={{color:"black"}}>{this.props.text}</h1>
-            <ul style={{color:"black"}}>{listItems}</ul>
-            {/* <p style={{color:"black"}}>{this.props.fnames}</p> */}
-          <button onClick={this.props.closePopup}>close me</button>
-          </div>
+            <ul >{filenames.map((filename) => <a href= "#" id={filename} key={filename.toString()} onClick={()=>this.download(filename)} > <li style={{color:"white"}}> {filename} </li></a>  )}</ul> 
+          </div>  
+        }
+
+      {boolfromupload && this.state.render &&    
+                <div className='popup_inner'>
+                <ul >{filenames.map((filename) => <a href= "#" id={filename} key={filename.toString()} onClick={()=>this.download(filename)} > <li style={{color:"black"}}> {filename} </li></a>  )}
+                </ul> 
+          </div>     
+      }
+
         </div>
       );
     }
